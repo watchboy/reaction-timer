a85c5b9ff763af9350bfe9e34d942260e1f04cfb
watchboy <kingjunkyeom@gmail.com>
2022-03-08 22:40:51 +0900

    202203082240

---
+++ src/components/Block.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reationTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+      console.log(this.delay)
+    }, this.delay)
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reationTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.$emit('end', this.reationTime)
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>Reaction time : {{score}} ms</p>
+  <p class = "rank"> {{rank}}</p>
+  
+</template>
+
+<script>
+export default {
+    props : ["score"],
+    data(){
+        return {
+            rank: null
+    
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = "고수"
+        }else if (this.score < 400){
+            this.rank = "킹반인"
+        }else if (this.score<600){
+            this.rank = "일반인"
+        }else{
+            this.rank = "느려"
+        }
+    }
+
+
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+</style>
